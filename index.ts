7b530c708716b6087c16f00e600da484e514c496
JM REMEUR <jm@remeur.fr>
2020-11-27 10:35:15 +0100

    encode fix header and payload data

--- index.ts
+++ index.ts
@@ -1,24 +1,21 @@
 const crypto = require('crypto');
 const jwa = require('jwa');
+const jws = require('jws');
 import { sign, SignOptions, verify } from 'jsonwebtoken';
 
-let header = {
-	'algorithm': 'sha256',
+let header: SignOptions = {
+	algorithm: 'HS256',
 }
 let payload = {
 	'name': 'Projet JWT'
 }
 let key = 'private_key';
 
-let options: SignOptions = {
-	'algorithm': 'HS256',
-}
-
 interface Options {
 	algorithm: string;
 }
 
-let token = sign(payload, key, options);
+let token = sign(payload, key, header);
 
 console.log(token);
 
@@ -36,13 +33,23 @@ function base64url(string: string, encoding: BufferEncoding = 'utf8'): string
 	  .replace(/\//g, '_');
 }
 
-function encodeJWT(payload: string | Buffer | object, 
-	key: string, header: Options): string
+function encodeJWT(payloadData: string | Buffer | object, 
+	key: string, headerData: SignOptions): string
 {
+	let header = {
+		alg: headerData.algorithm,
+		typ: 'JWT'
+	}
+
+	let payload = Object.assign({
+		iat: Math.floor(Date.now() / 1000)
+	}, payloadData);
+
+	let algo = header.alg?.substr(header.alg.length - 3);
 	let base64Header = base64url(JSON.stringify(header), 'binary');
 	let base64Payload = base64url(JSON.stringify(payload));
 	let input = `${base64Header}.${base64Payload}`
-	let hmac = crypto.createHmac(header.algorithm, key);
+	let hmac = crypto.createHmac(`sha${algo}`, key);
 	let hash = (hmac.update(input), hmac.digest('base64'));
 	let signature = base64url(JSON.stringify(hash));
 
@@ -61,6 +68,3 @@ console.log(encoded);
 let decode = verify(token, key);
 
 console.log(decode);
-
-const hmac = jwa('HS256');
-console.log(hmac.sign(payload, key))
