d11ba30d8fa5af13430dc024df697ccdadbe2446
JM REMEUR <jm@remeur.fr>
2020-11-27 11:40:59 +0100

    export sign and verify

--- index.ts
+++ index.ts
@@ -1,43 +1,60 @@
-const crypto = require('crypto');
-const jwa = require('jwa');
-const jws = require('jws');
-import { sign, SignOptions, verify } from 'jsonwebtoken';
+import crypto from 'crypto';
 
-const header: SignOptions = {
-	algorithm: 'HS256',
-}
-const payload = {
-	'name': 'Projet JWT'
-}
-const key = 'private_key';
+type Algorithm =
+    "HS256" | "HS384" | "HS512" |
+    "RS256" | "RS384" | "RS512" |
+    "ES256" | "ES384" | "ES512" |
+    "PS256" | "PS384" | "PS512" |
+	"none";
 
-interface Options {
-	algorithm: string;
+interface SignOptions {
+	algorithm?: Algorithm
 }
 
-const token = sign(payload, key, header);
-
-console.log(token);
-
-const decoded = verify(token, key);
-
-console.log(decoded);
-
-function base64url(string: string, encoding: BufferEncoding = 'utf8'): string 
+function base64url(str: string, encoding: BufferEncoding = 'utf8'): string
 {
 	return Buffer
-	  .from(string, encoding)
+	  .from(str, encoding)
 	  .toString('base64')
 	  .replace(/=/g, '')
 	  .replace(/\+/g, '-')
 	  .replace(/\//g, '_');
 }
 
-function encodeJWT(payloadData: string | Buffer | object,
-	key: string, headerData: SignOptions): string
+function fromBase64(base64: string)
+{
+	return base64
+		.replace(/=/g, '')
+		.replace(/\+/g, '-')
+		.replace(/\//g, '_');
+}
+
+function decodeBase64(str: string, encoding: BufferEncoding = 'utf8'): string
+{
+	return Buffer.from(str, 'base64').toString(encoding);
+}
+
+function toString(obj: any)
+{
+	if (typeof obj === 'string')
+		return obj;
+	if (typeof obj === 'number' || Buffer.isBuffer(obj))
+		return obj.toString();
+	return JSON.stringify(obj);
+};
+
+function createSignature(algo: string | undefined, secret: string, input: string): string
+{
+	const hmac = crypto.createHmac(`sha${algo}`, secret);
+	const hash = (hmac.update(input), hmac.digest('base64'));
+	return fromBase64(toString(hash));
+}
+
+export function sign(payloadData: string | Buffer | object,
+	secret: string, headerData: SignOptions): string
 {
 	const header = {
-		alg: headerData.algorithm,
+		alg: headerData.algorithm ?? 'HS256',
 		typ: 'JWT'
 	}
 
@@ -46,25 +63,29 @@ function encodeJWT(payloadData: string | Buffer | object,
 	}, payloadData);
 
 	const algo = header.alg?.substr(header.alg.length - 3);
-	const base64Header = base64url(JSON.stringify(header), 'binary');
-	const base64Payload = base64url(JSON.stringify(payload));
-	const input = `${base64Header}.${base64Payload}`
-	const hmac = crypto.createHmac(`sha${algo}`, key);
-	const hash = (hmac.update(input), hmac.digest('base64'));
-	const signature = base64url(JSON.stringify(hash));
+	const base64Header = base64url(toString(header), 'binary');
+	const base64Payload = base64url(toString(payload));
+	const input = `${base64Header}.${base64Payload}`;
+	const signature = createSignature(algo, secret, input);
 
 	return `${input}.${signature}`;
 }
 
-/*function decodeJWT(): string | object
+export function verify(token: string, secret: string): string | object
 {
+	const split = token.split('.');
 
-}*/
+	const header = JSON.parse(decodeBase64(split[0], 'binary'));
+	const algo = header.alg?.substr(header.alg.length - 3);
+	const payload =  JSON.parse(decodeBase64(split[1]));
 
-const encoded = encodeJWT(payload, key, header);
+	const input = `${split[0]}.${split[1]}`;
+	const signature = split[2];
+	const createdSignature = createSignature(algo, secret, input);
 
-console.log(encoded);
+	if(signature !== createdSignature) {
+		throw new Error('Invalid JWT token')
+	}
 
-const decode = verify(token, key);
-
-console.log(decode);
+	return payload;
+}
